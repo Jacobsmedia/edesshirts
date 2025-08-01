150915c22567d5d59a0e0d7695ce062365f2e8a2
Jacobsmedia <jacobsmedia12@gmail.com>
2025-08-01 20:43:39 +0200

    Improve mobile nav

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const navLinks = document.querySelector('.nav-links');
   if (mobileMenu && navLinks) {
     mobileMenu.addEventListener('click', function () {
+      mobileMenu.classList.toggle("open");
       navLinks.classList.toggle('open');
     });
   }
